d9436abed56b229d3bd9d9ed0d8382fbe7d75726
bcgpt <136483921+bcgpt@users.noreply.github.com>
2023-06-23 17:07:45 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -176,7 +176,7 @@ export function SideBar(props: { className?: string }) {
             </Link>
           </div>
           <div className={styles["sidebar-action"]}>
-            <a href={https://www.bcgpt.cc} target="_blank">
+            <a href={} target="_blank">
               <IconButton icon={<GithubIcon />} shadow />
             </a>
           </div>
